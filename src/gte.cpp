b3cbe5556b70ded16433f620e08ea5c8f45bbc69
yanming <yanming-sohu@sohu.com>
2020-05-28 20:00:41 +0800

    GTE GTE GTE

--- src/gte.cpp
+++ src/gte.cpp
@@ -3,6 +3,61 @@
 namespace ps1e {
 
 
+GTE::GTE() : 
+  ir1(*this,  0, GteReg63Error::Ir1), ir2(*this, 5, GteReg63Error::Ir2), 
+  ir3(*this, 15, GteReg63Error::Ir3), ir0(*this, 0, GteReg63Error::Ir0), 
+  irgb(*this), orgb(*this),
+  sxyp(*this), sz3(*this), rgb2(*this), lzcs(*this),
+  mac0(*this, GteReg63Error::Mac0p), mac1(*this, GteReg63Error::Mac1p),
+  mac2(*this, GteReg63Error::Mac2p), mac3(*this, GteReg63Error::Mac3p),
+
+  r32(rt.r11, rt.r12), r33(rt.r13, rt.r21), r34(rt.r22, rt.r23),
+  r35(rt.r31, rt.r32), r36(rt.r33),
+
+  r40(llm.r11, llm.r12), r41(llm.r13, llm.r21), r42(llm.r22, llm.r23),
+  r43(llm.r31, llm.r32), r44(llm.r33),
+
+  r48(lcm.lr1, lcm.lr2), r49(lcm.lg3, lcm.lg1), r50(lcm.lg2, lcm.lg3),
+  r51(lcm.lb1, lcm.lb2), r52(lcm.lb3)
+{}
+
+
+GteMac::GteMac(GTE& g, GteReg63Error p) : r(g), positive(p) {
+}
+
+
+u32 GteMac::read() {
+  return (s32) v;
+}
+
+
+void GteMac::write(u32 d) {
+  v = (s32)d;
+}
+
+
+GteIR::GteIR(GTE& _r, int bit, GteReg63Error e) : r(_r), Lbit(bit), IRE(e) {
+}
+
+
+u32 GteIR::read() {
+  return v;
+}
+
+
+void GteIR::write(u32 _v) {
+  v = _v;
+  u32 s = v >> 3;
+  u32 mask = 0x1f << Lbit;
+  r.irgb.v = (r.irgb.v & mask) | (s << Lbit);
+}
+
+
+void GteIR0::write(u32 d) {
+  v = d;
+}
+
+
 void GteIrgb::write(u32 d) {
   v = d;
   r.ir1.v = (0x1f & v) << 3;
@@ -12,7 +67,7 @@ void GteIrgb::write(u32 d) {
 
 
 u32 GteOrgb::read() {
-  return i.v;
+  return r.irgb.v;
 }
 
 
@@ -23,17 +78,26 @@ void GteFlag::write(u32 d) {
 
 void GteFlag::set(GteReg63Error f) {
   v |= static_cast<u32>(f);
+}
+
+
+void GteFlag::update() {
   if (v & GteFlagLogSum) {
-    v |= (1 << 31);
+    v |= (1u << 31);
   } else {
-    v &= (1 << 31)-1;
+    v &= (1u << 31)-1;
   }
 }
 
 
+void GteFlag::clear() {
+  v = 0;
+}
+
+
 void GteVectorXY::write(u32 d) {
-  fx = (s32) (d & 0xffff);
-  fy = (s32) ((d>>16) & 0xffff);
+  fx = (s32) ((s16)(d & 0xffff));
+  fy = (s32) ((s16)((d>>16) & 0xffff));
 }
 
 
@@ -41,6 +105,461 @@ u32 GteVectorXY::read() {
   s16 x = fx;
   s16 y = fy;
   return x & (((s32) y) << 16);
+}
+
+
+GteSxyFifo::GteSxyFifo(GTE& _r) : r(_r) {
+}
+
+
+u32 GteSxyFifo::read() {
+  return r.sxy2.read();
+}
+
+
+void GteSxyFifo::write(u32 d) {
+  float x = (s32) ((s16)(d & 0xffff));
+  float y = (s32) ((s16)((d>>16) & 0xffff));
+  push(x, y);
+}
+
+
+void GteSxyFifo::push(float x, float y) {
+  r.sxy0.fx = r.sxy1.fx;
+  r.sxy0.fy = r.sxy1.fy;
+  r.sxy1.fx = r.sxy2.fx;
+  r.sxy1.fy = r.sxy2.fy;
+  r.sxy2.fx = x;
+  r.sxy2.fy = y;
+}
+
+
+GteZFifo::GteZFifo(GTE& _r) : r(_r) {
+}
+
+
+u32 GteZFifo::read() {
+  return (s16) v;
+}
+
+
+void GteZFifo::write(u32 d) {
+  push(s16(d));
+}
+
+
+void GteZFifo::push(float d) {
+  r.sz0.v = r.sz1.v;
+  r.sz1.v = r.sz2.v;
+  r.sz2.v = v;
+  v = d;
+}
+
+
+GteRgbFifo::GteRgbFifo(GTE& _r) : r(_r) {
+}
+
+
+u32 GteRgbFifo::read() {
+  return v;
+}
+
+
+void GteRgbFifo::write(u32 d) {
+  r.rgb0.v = r.rgb1.v;
+  r.rgb1.v = v;
+  v = d;
+}
+
+
+GteMatrixReg::GteMatrixReg(float& lsb, float& msb) : m(msb), l(lsb) {
+}
+
+
+u32 GteMatrixReg::read() {
+  s16 lsb = l;
+  s16 msb = m;
+  return u32(u16(msb) << 16) | u16(lsb);
+}
+
+
+void GteMatrixReg::write(u32 d) {
+  s16 lsb = (s16) (0xffff & d);
+  s16 msb = (s16) (0xffff & (d >> 16));
+  l = lsb;
+  m = msb;
+}
+
+
+GteMatrixReg1::GteMatrixReg1(float& p) : v(p) {
+}
+
+
+u32 GteMatrixReg1::read() {
+  return (s16) v;
+}
+
+
+void GteMatrixReg1::write(u32 d) {
+  v = (s16) d;
+}
+
+
+GteLeadingZeroes::GteLeadingZeroes(GTE& _r) : r(_r) {
+}
+
+
+u32 GteLeadingZeroes::read() {
+  return v;
+}
+
+
+//TODO: 结果在 1..32 范围内?
+void GteLeadingZeroes::write(u32 d) {
+  v = d;
+
+  if (d & 0x8000'0000) d = ~d;
+  if (d == 0) {
+    r.lzcr.v = 32;
+    return;
+  }
+  u32 mask = 0x8000'0000;
+  for (u32 i = 0; i < 32; ++i) {
+    if (mask & d) {
+      r.lzcr.v = i;
+      return;
+    }
+    mask = mask >> 1;
+  }
+}
+
+
+// fn(RegNumber, RegName, data)
+#define GTE_DATA_REG_LIST(fn, d) \
+  fn( 0, vxy0, d) \
+  fn( 1, vz0,  d) \
+  fn( 2, vxy1, d) \
+  fn( 3, vz1,  d) \
+  fn( 4, vxy2, d) \
+  fn( 5, vz2,  d) \
+  fn( 6, rgbc, d) \
+  fn( 7, otz,  d) \
+  fn( 8, ir0,  d) \
+  fn( 9, ir1,  d) \
+  fn(10, ir2,  d) \
+  fn(11, ir3,  d) \
+  fn(12, sxy0, d) \
+  fn(13, sxy1, d) \
+  fn(14, sxy2, d) \
+  fn(15, sxyp, d) \
+  fn(16, sz0,  d) \
+  fn(17, sz1,  d) \
+  fn(18, sz2,  d) \
+  fn(19, sxyp, d) \
+  fn(20, rgb0, d) \
+  fn(21, rgb1, d) \
+  fn(22, rgb2, d) \
+  fn(23, res1, d) \
+  fn(24, mac0, d) \
+  fn(25, mac1, d) \
+  fn(26, mac2, d) \
+  fn(27, mac3, d) \
+  fn(28, irgb, d) \
+  fn(29, orgb, d) \
+  fn(30, lzcs, d) \
+  fn(31, lzcr, d)
+
+#define GTE_CTRL_REG_LIST(fn, d) \
+  fn( 0, r32,  d) \
+  fn( 1, r33,  d) \
+  fn( 2, r34,  d) \
+  fn( 3, r35,  d) \
+  fn( 4, r36,  d) \
+  fn( 5, trX,  d) \
+  fn( 6, trY,  d) \
+  fn( 7, trZ,  d) \
+  fn( 8, r40,  d) \
+  fn( 9, r41,  d) \
+  fn(10, r42,  d) \
+  fn(11, r43,  d) \
+  fn(12, r44,  d) \
+  fn(13, rbk,  d) \
+  fn(14, gbk,  d) \
+  fn(15, bbk,  d) \
+  fn(16, r48,  d) \
+  fn(17, r49,  d) \
+  fn(18, r50,  d) \
+  fn(19, r51,  d) \
+  fn(20, r52,  d) \
+  fn(21, rfc,  d) \
+  fn(22, gfc,  d) \
+  fn(23, bfc,  d) \
+  fn(24, offx, d) \
+  fn(25, offy, d) \
+  fn(26, H,    d) \
+  fn(27, dqa,  d) \
+  fn(28, dqb,  d) \
+  fn(29, zsf3, d) \
+  fn(30, zsf4, d) \
+  fn(31, flag, d)
+
+#define GTE_WRITE_CASE(i, r, d)   case i: r.write(d); break;
+#define GTE_READ_CASE(i, r, _)    case i: return r.read();
+
+
+u32 GTE::read_data(const u32 i) {
+  switch (i) {
+    GTE_DATA_REG_LIST(GTE_READ_CASE, 0)
+    default:
+      error("Invaild GTE data reg %d\n", i);
+      return 0;
+  }
+}
+
+
+void GTE::write_data(const u32 i, const u32 d) {
+  switch (i) {
+    GTE_DATA_REG_LIST(GTE_WRITE_CASE, d)
+    default:
+      error("Invaild GTE data reg %d\n", i);
+  }
+}
+
+
+u32 GTE::read_ctrl(const u32 i) {
+  switch (i) {
+    GTE_CTRL_REG_LIST(GTE_READ_CASE, 0)
+    default:
+      error("Invaild GTE ctrl reg %d\n", i);
+      return 0;
+  }
+}
+
+
+void GTE::write_ctrl(const u32 i, const u32 d) {
+  switch (i) {
+    GTE_CTRL_REG_LIST(GTE_WRITE_CASE, d)
+    default:
+      error("Invaild GTE ctrl reg %d\n", i);
+  }
+}
+
+
+// fn(CommandNum, Function, GteCommand)
+#define GTE_COMMAND_LIST(fn, c) \
+  fn(0x01, RTPS,  c) \
+  fn(0x06, NCLIP, c) \
+  fn(0x0C, OP,    c) \
+  fn(0x10, DPCS,  c) \
+  fn(0x11, INTPL, c) \
+  fn(0x12, MVMVA, c) \
+  fn(0x13, NCDS,  c) \
+  fn(0x14, CDP,   c) \
+  fn(0x16, NCDT,  c) \
+  fn(0x1B, NCCS,  c) \
+  fn(0x1C, CC,    c) \
+  fn(0x1E, NCS,   c) \
+  fn(0x20, NCT,   c) \
+  fn(0x28, SQR,   c) \
+  fn(0x29, DCPL,  c) \
+  fn(0x2A, DPCT,  c) \
+  fn(0x2D, AVSZ3, c) \
+  fn(0x2E, AVSZ4, c) \
+  fn(0x30, RTPT,  c) \
+  fn(0x3D, GPF,   c) \
+  fn(0x3E, GPL,   c) \
+  fn(0x3F, NCCT,  c)
+
+#define GTE_CMD_CASE(n, f, c)   case n: f(c); break;
+
+
+bool GTE::execute(const GteCommand c) {
+  // 如果执行无效指令, flag 也被清除
+  flag.clear();
+  switch (c.num) {
+    GTE_COMMAND_LIST(GTE_CMD_CASE, c);
+    default:
+      return false;
+  }
+  flag.update();
+  return true;
+}
+
+
+void GTE::write_ir(GteIR& ir, s32 v, u32 lm) {
+  if (lm) {
+    if (v < 0) {
+      flag.set(ir.IRE);
+      v = 0;
+    } else if (v > GteOF15) {
+      flag.set(ir.IRE);
+      v = GteOF15-1;
+    }
+  } else {
+    if (v < -GteOF15) {
+      flag.set(ir.IRE);
+      v = -GteOF15;
+    } else if (v > GteOF15) {
+      flag.set(ir.IRE);
+      v = GteOF15-1;
+    }
+  }
+  ir.write(v);
+}
+
+
+void GTE::write_ir0(s32 v, u32) {
+  if (v < 0) {
+    flag.set(ir0.IRE);
+    v = 0;
+  } else if (v > GteOf12) {
+    flag.set(ir0.IRE);
+    v = GteOf12-1;
+  }
+  ir0.write(v);
+}
+
+
+void GTE::write_mac(GteMac& mac, double d) {
+  if (d > GteOF43) {
+    flag.set(mac.positive);
+  }
+  else if (d < -GteOF43) {
+    flag.set(static_cast<GteReg63Error>(static_cast<u32>(mac.positive) + 3));
+  }
+  mac.v = d;
+}
+
+
+void GTE::write_mac0(double d) {
+  if (d > GteOF31) {
+    flag.set(mac0.positive);
+  }
+  else if (d < -GteOF31) {
+    flag.set(static_cast<GteReg63Error>(static_cast<u32>(mac0.positive) + 3));
+  }
+  mac0.v = d;
+}
+
+
+void GTE::write_color_fifo() {
+  u32 r = mac1.read() >> 4;
+  u32 g = mac2.read() >> 4;
+  u32 b = mac3.read() >> 4;
+  u32 c = rgbc.v;
+
+  if (r > 0xff) {
+    r = 0xff;
+    flag.set(GteReg63Error::R);
+  }
+  if (g > 0xff) {
+    g = 0xff;
+    flag.set(GteReg63Error::G);
+  }
+  if (b > 0xff) {
+    b = 0xff;
+    flag.set(GteReg63Error::B);
+  }
+  rgb2.write(r | (g << 8) | (b << 16) | (c << 24));
+}
+
+
+void GTE::write_z_fifo(float d) {
+  if (d > 0xFFFF) {
+    d = 0xffff;
+    flag.set(GteReg63Error::Sz3);
+  }
+  else if (d < 0) {
+    d = 0;
+    flag.set(GteReg63Error::Sz3);
+  }
+  sz3.push(d);
+}
+
+
+void GTE::write_otz(float d) {
+  if (d > 0xFFFF) {
+    d = 0xffff;
+    flag.set(GteReg63Error::Sz3);
+  }
+  else if (d < 0) {
+    d = 0;
+    flag.set(GteReg63Error::Sz3);
+  }
+  otz.v = d;
+}
+
+
+void GTE::write_xy_fifo(float x, float y) {
+  if (x > 0x3ff) {
+    x = 0x3ff;
+    flag.set(GteReg63Error::Sx2);
+  } else if (x < -0x400) {
+    x = -0x400;
+    flag.set(GteReg63Error::Sx2);
+  }
+
+  if (y > 0x3ff) {
+    y = 0x3ff;
+    flag.set(GteReg63Error::Sy2);
+  } else if (y > -0x400) {
+    y = -0x400;
+    flag.set(GteReg63Error::Sy2);
+  }
+
+  sxyp.push(x, y);
+}
+
+
+float GTE::overflow_h() {
+  float r = (( (H.v * 0x20000 / sz3.v) +1) /2);
+  if (r > 0x1FFFF) {
+    r = 0x1FFFF;
+    flag.set(GteReg63Error::Div);
+  }
+  return r;
+}
+
+
+void GTE::RTPS(GteCommand c) {
+  PerspectiveTransform(c, sxy0, sz0);
+}
+
+
+void GTE::RTPT(GteCommand c) {
+  PerspectiveTransform(c, sxy0, sz0);
+  PerspectiveTransform(c, sxy1, sz1);
+  PerspectiveTransform(c, sxy2, sz2);
+}
+
+
+void GTE::PerspectiveTransform(GteCommand c, GteVectorXY& xy, GteVectorZ& z) {
+  double tr = (trX.v * 0x1000);
+  double m1 = tr +(rt.r11 * xy.fx)+(rt.r12 * xy.fy)+(rt.r13 * z.v);
+  double m2 = tr +(rt.r21 * xy.fx)+(rt.r22 * xy.fy)+(rt.r23 * z.v);
+  double m3 = tr +(rt.r31 * xy.fx)+(rt.r32 * xy.fy)+(rt.r33 * z.v);
+  if (c.sf) {
+    m1 /= 0x1000;
+    m2 /= 0x1000;
+    m3 /= 0x1000;
+    write_z_fifo(m3);
+  } else {
+    write_z_fifo(m3 / 0x1000);
+  }
+  write_mac(mac1, m1);
+  write_mac(mac2, m2);
+  write_mac(mac3, m3);
+  write_ir(ir1, m1, c.lm);
+  write_ir(ir2, m2, c.lm);
+  write_ir(ir3, m3, c.lm);
+
+  float h = overflow_h();
+  float x = (h * ir1.v + offx.v) / 0x10000;
+  float y = (h * ir2.v + offy.v) / 0x10000;
+  write_xy_fifo(x, y);
+
+  write_mac0(h * dqa.v + dqb.v);
+  write_ir0(mac0.v / 0x1000);
 }
 
 
--- src/gte.h
+++ src/gte.h
@@ -6,10 +6,17 @@ namespace ps1e {
 
 #define GteReg63WriteMask 0x7FFFF000
 #define GteFlagLogSum     0x7F87E000
+#define GteCommandFix     0x4A000000
+#define GteOF43           (static_cast<long long>(1) << 43)
+#define GteOF31           (static_cast<long long>(1) << 31)
+#define GteOF15           (1 << 15)
+#define GteOf12           (1 << 12)
+
+class GTE;
 
 
 enum class GteReg63Error : u32 {
-  Error = 1<<31, // (Bit30..23, and 18..13 ORed together) (Read only)
+  Error = 1u<<31,// (Bit30..23, and 18..13 ORed together) (Read only)
   Mac1p = 1<<30, // MAC1结果大于43位且为正
   Mac2p = 1<<29, // MAC2 Result larger than 43 bits and positive
   Mac3p = 1<<28,
@@ -32,9 +39,47 @@ enum class GteReg63Error : u32 {
 };
 
 
-typedef u32 GteDataRegisterType;
-typedef u32 GteCtrlRegisterType;
-class GteDataReg;
+union GteCommand {
+  u32 v;
+  struct {
+    u32 num     : 6; //00-05 Real GTE Command Number (00h..3Fh)
+    u32 _z1     : 4; //06-09
+    u32 lm      : 1; //10    Saturate IR1,IR2,IR3 result (0=To -8000h..+7FFFh, 1=To 0..+7FFFh)
+    u32 _z2     : 2; //11,12
+    u32 mv_tv   : 2; //13,14 see 'MVMVA_Trans_Vec'
+    u32 mv_mv   : 2; //15,16 see 'MVMVA_Mul_Vec'
+    u32 mv_mm   : 2; //17,18 see 'MVMVA_Mul_Mx'
+    u32 sf      : 1; //19    Shift Fraction in IR registers (0=No fraction, 1=12bit fraction)
+    u32 fake    : 5; //20-24 Fake GTE Command Number (00h..1Fh) (ignored by hardware)
+    u32 _fix    : 7; //25-31 Must be 0100101b for "COP2 imm25" instructions, 'GteCommandFix'
+  };
+
+  GteCommand(u32 i) : v(i) {}
+};
+
+
+enum class MVMVA_Mul_Mx : u32 {
+  rotation = 0,
+  light = 1,
+  color = 2,
+  reserved = 3,
+};
+
+
+enum class MVMVA_Mul_Vec : u32 {
+  v0 = 0,
+  v1 = 1,
+  v2 = 2,
+  ir = 3,
+};
+
+
+enum class MVMVA_Trans_Vec : u32 {
+  tr = 0,
+  bk = 1,
+  fc = 2,
+  none = 3,
+};
 
 
 struct GteFlag {
@@ -43,6 +88,8 @@ struct GteFlag {
   u32 read() { return v; }
   void write(u32 d);
   void set(GteReg63Error f);
+  void clear();
+  void update();
 };
 
 
@@ -55,77 +102,175 @@ struct GteVectorXY {
 
 
 struct GteVectorZ {
-  float fz;
+  float v;
 
-  void write(u32 z) { fz = (s32) z; }
-  u32 read() { return (s32) fz; }
+  void write(u32 z) { v = (s16) z; }
+  u32 read() { return (s16) v; }
 };
 
 
 struct GteRgb {
-  u32 rgb;
+  u32 v;
 
-  void write(u32 r) { rgb = r; }
-  u32 read() { return rgb; }
+  void write(u32 r) { v = r; }
+  u32 read() { return v; }
 };
 
 
-template<int Lbit, GteReg63Error IRE> 
-struct GteIR {
-  GteDataReg& r;
+struct GteRgbFifo {
   u32 v;
+  GTE& r;
 
-  GteIR(GteDataReg& _r) : r(_r) {}
+  GteRgbFifo(GTE&);
+  void write(u32);
+  u32 read();
+};
 
-  u32 read() { return v; }
 
-  void write(u32 _v) {
-    v = _v;
-    u8 s;
-    if (v & 0x8000) {
-      s = 0;
-      r.flag.set(IRE);
-    } else if (v & 0x7000) {
-      s = 0x1f;
-      r.flag.set(IRE);
-    } else {
-      s = v >> 3;
-    }
-    u32 mask = 0x1f << Lbit;
-    r.irgb.rgb = (r.irgb.rgb & mask) | (s << Lbit);
-  }
+struct GteIR {
+  s32 v;
+
+  const int Lbit;
+  const GteReg63Error IRE;
+  GTE& r;
+
+  GteIR(GTE& _r, int bit, GteReg63Error e);
+  virtual ~GteIR() {}
+  u32 read();
+  void write(u32 _v);
+};
+
+
+struct GteIR0 : public GteIR {
+using GteIR::GteIR;
+  void write(u32);
 };
 
 
 struct GteIrgb {
   u32 v;
-  GteDataReg& r;
+  GTE& r;
 
-  GteIrgb(GteDataReg& _r) : r(_r) {}
+  GteIrgb(GTE& _r) : r(_r) {}
   u32 read() { return 0; }
   void write(u32 _v);
 };
 
 
 struct GteOrgb {
-  GteIrgb &i;
+  GTE &r;
 
-  GteOrgb(GteIrgb& _i) : i(_i) {}
-
+  GteOrgb(GTE& _r) : r(_r) {}
   void write(u32) {}
   u32 read();
 };
 
 
-struct GteFloat {
-  float r;
+// 没有副作用的原生寄存器
+template<class Save, class Real>
+struct GteSrcReg {
+  Save v;
 
-  u32 read() { return (s32) r; }
-  void write(u32 v) { r = (s32) v; }
+  u32 read() { return (Real) v; }
+  void write(u32 d) { v = (Real) d; }
 };
 
 
-class GteReg {
+struct GteMac {
+  double v;
+  GTE& r;
+  const GteReg63Error positive;
+
+  GteMac(GTE&, GteReg63Error);
+  virtual ~GteMac() {}
+  u32 read();
+  void write(u32 d);
+};
+
+
+struct GteMac0 : public GteMac {
+using GteMac::GteMac;
+};
+
+
+struct GteSxyFifo {
+  GTE& r;
+
+  GteSxyFifo(GTE&);
+  u32 read();
+  void write(u32 v);
+  void push(float x, float y);
+};
+
+
+struct GteZFifo {
+  GTE& r;
+  float v;
+
+  GteZFifo(GTE&);
+  u32 read();
+  void write(u32 v);
+  void push(float);
+};
+
+
+union GteMatrix {
+  float v[9];
+  struct {
+    float r11, r12, r13;
+    float r21, r22, r23;
+    float r31, r32, r33;
+  };
+  struct {
+    float lr1, lr2, lr3;
+    float lg1, lg2, lg3;
+    float lb1, lb2, lb3;
+  };
+};
+
+
+struct GteMatrixReg {
+  float &m, &l;
+
+  // | 31 <--- MSB ---> 16 | 15 <--- LSB ---> 0 |
+  GteMatrixReg(float& lsb, float& msb);
+  u32 read();
+  void write(u32 v);
+};
+
+
+struct GteMatrixReg1 {
+  float &v;
+
+  GteMatrixReg1(float &p);
+  u32 read();
+  void write(u32 v);
+};
+
+
+template<class T>
+struct GteReadonly {
+  T v;
+  u32 read() { return v; }
+  void write(u32) {}
+};
+
+
+struct GteLeadingZeroes {
+  s32 v;
+  GTE& r;
+
+  GteLeadingZeroes(GTE&);
+  u32 read();
+  void write(u32 v);
+};
+
+
+struct GteOTZ : public GteSrcReg<float, u16>  {
+};
+
+
+class GTE {
 private:
   // r0-r1 Vector 0 (X,Y,Z)
   GteVectorXY vxy0;
@@ -137,53 +282,135 @@ private:
   GteVectorXY vxy2;
   GteVectorZ  vz2; 
   // r6 4xU8 Color/code value
-  GteDataRegisterType rgbc;      
+  GteRgb rgbc;      
   // r7 Average Z value (for Ordering Table)
-  GteDataRegisterType otz;       
+  GteOTZ otz;       
   // r8 16bit Accumulator (Interpolate)
-  GteFloat ir0;
+  GteIR0 ir0;
   // r9-r11 16bit Accumulator (Vector)
-  GteIR<0, GteReg63Error::Ir1> ir1; 
-  GteIR<5, GteReg63Error::Ir2> ir2;
-  GteIR<10, GteReg63Error::Ir3> ir3;
+  GteIR ir1, ir2, ir3; 
   // r12-r15 Screen XY-coordinate FIFO  (3 stages)
-  GteDataRegisterType sxy0, sxy1, sxy2, sxyp;
+  GteVectorXY sxy0, sxy1, sxy2;
+  GteSxyFifo sxyp;
   // r16-r19 Screen Z-coordinate FIFO   (4 stages)
-  GteDataRegisterType sz0, sz1, sz2, sz3;
+  GteVectorZ sz0, sz1, sz2;
+  GteZFifo sz3;
   // r20-r22 Color CRGB-code/color FIFO (3 stages)
-  GteDataRegisterType rgb0, rgb1, rgb2;
-  // Prohibited
-  GteDataRegisterType res1;
+  GteRgb rgb0, rgb1;
+  GteRgbFifo rgb2;
+  // r23 Prohibited
+  GteRgb res1;
   // r24 32bit Maths Accumulators (Value)
-  GteDataRegisterType mac0;
+  GteMac0 mac0;
   // r25-27 32bit Maths Accumulators (Vector)
-  GteDataRegisterType mac1, mac2, mac3;
+  GteMac mac1, mac2, mac3;
   // r28-r29 Convert RGB Color (48bit vs 15bit)
-  GteDataRegisterType irgb, orgb;
+  GteIrgb irgb;
+  GteOrgb orgb;
   // r30-r31 Count Leading-Zeroes/Ones (sign bits)
-  GteDataRegisterType lzcs, lzcr;
+  GteLeadingZeroes lzcs;
+  GteReadonly<u32> lzcr;
 
-  // r63/cnt31 Returns any calculation errors
+  // r32-r36 / cnt0-4 Rotation matrix(3x3)
+  GteMatrix rt;
+  GteMatrixReg r32;
+  GteMatrixReg r33;
+  GteMatrixReg r34;
+  GteMatrixReg r35;
+  GteMatrixReg1 r36;
+  // r37-r39 / cnt5-7 Translation vector (X,Y,Z) 
+  GteSrcReg<float, s32> trX, trY, trZ;
+  // r40-r44 / cnt8-12 Light source matrix
+  GteMatrix llm;
+  GteMatrixReg r40;
+  GteMatrixReg r41;
+  GteMatrixReg r42;
+  GteMatrixReg r43;
+  GteMatrixReg1 r44;
+  // r45-r47 / cnt13-15 Background color
+  GteSrcReg<float, u32> rbk, gbk, bbk;
+  // r48-r52 / cnt16-20 Light color matrix source
+  GteMatrix lcm;
+  GteMatrixReg r48;
+  GteMatrixReg r49;
+  GteMatrixReg r50;
+  GteMatrixReg r51;
+  GteMatrixReg1 r52;
+  // r53-r55 / cnt21-23 Far color
+  GteSrcReg<float, u32> rfc, gfc, bfc;
+  // r56,r57 / cnt24-25 Screen offset
+  GteSrcReg<float, u32> offx, offy;
+  // r58 / cnt26 Projection plane distance
+  // 读取H寄存器时，硬件意外地对<unsigned> 16bit值进行了 <sign-expand>
+  //（即，值 +8000h..+FFFFh 返回为 FFFF8000h..FFFFFFFFh）此错误仅适用于 `mov rd`
+  GteSrcReg<float, u16> H;
+  // r59 / cnt27 Depth queing parameter A (coeff)
+  GteSrcReg<s16, s16> dqa;
+  // r60 / cnt28 Depth queing parameter B (offset)
+  GteSrcReg<float, s32> dqb;
+  // r61,r62 / cnt29-30 Average Z scale factors
+  GteSrcReg<s16, s16> zsf3, zsf4;
+  // r63 / cnt31 Returns any calculation errors
   GteFlag flag;
 
 public:
-  GteReg();
+  GTE();
 
-  void write_data(u32 index, u32 data);
-  u32 read_data(u32 index);
+  void write_data(const u32 index, const u32 data);
+  u32 read_data(const u32 index);
 
-  void write_ctrl(u32 index, u32 data);
-  u32 read_ctrl(u32 index);
+  void write_ctrl(const u32 index, const u32 data);
+  u32 read_ctrl(const u32 index);
 
+  bool execute(const GteCommand c);
+
+private:
+  void RTPS(GteCommand);
+  void NCLIP(GteCommand);
+  void OP(GteCommand);
+  void DPCS(GteCommand);
+  void INTPL(GteCommand);
+  void MVMVA(GteCommand);
+  void NCDS(GteCommand);
+  void CDP(GteCommand);
+  void NCDT(GteCommand);
+  void NCCS(GteCommand);
+  void CC(GteCommand);
+  void NCS(GteCommand);
+  void NCT(GteCommand);
+  void SQR(GteCommand);
+  void DCPL(GteCommand);
+  void DPCT(GteCommand);
+  void AVSZ3(GteCommand);
+  void AVSZ4(GteCommand);
+  void RTPT(GteCommand);
+  void GPF(GteCommand);
+  void GPL(GteCommand);
+  void NCCT(GteCommand);
+
+private:
+  void PerspectiveTransform(GteCommand c, GteVectorXY& xy, GteVectorZ& z);
+
+  void write_ir(GteIR& ir, s32 d, u32 lm);
+  void write_ir0(s32 d, u32 lm = 0);
+  void write_mac(GteMac& mac, double d);
+  void write_mac0(double d);
+  // 从 mac123 读取, 并写入 fifo
+  void write_color_fifo();
+  void write_z_fifo(float d);
+  void write_otz(float d);
+  void write_xy_fifo(float x, float y);
+  // (( (H*20000h/SZ3) +1)/2)
+  float overflow_h();
+
+friend struct GteLeadingZeroes;
+friend struct GteIR;
+friend struct GteRgbFifo;
+friend struct GteZFifo;
 friend struct GteIrgb;
-template<int, GteReg63Error> friend struct GteIR;
+friend struct GteOrgb;
+friend struct GteSxyFifo;
 };
 
-
-union GteCtrlReg {
-  GteCtrlRegisterType c[32];
-  struct {
-  };
-};
 
 }
